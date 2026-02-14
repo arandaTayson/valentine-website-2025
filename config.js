ecc725b95746fa2bf318b68c564b0ab203316863
arandaTayson <celestenayeli.1998@gmail.com>
2026-02-14 01:31:11 -0500

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://raw.githubusercontent.com/ArandaTayson/valentine-website-2025/principal/m%C3%BAsica/coqueta.mp3", // Music streaming URL
+        musicUrl: "https://raw.githubusercontent.com/ArandaTayson/sitio-web-de-valentine-2025/principal/m%C3%BAsica/coqueta.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
